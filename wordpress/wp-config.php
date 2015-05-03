4e75560eeedf3af7abe9e691fa0824b34118098f
Lukas Juhas <hello@lukasjuhas.com>
2015-05-03 18:55:49 +0100

    Set up multisite
    set up multisite for german version of the site

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -59,6 +59,16 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/* Multisite */
+define( 'WP_ALLOW_MULTISITE', true );
+
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'o2studios.lukasjuhas.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
